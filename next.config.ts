68d2de68e6fa8a41333966f862b2a8015098ba65
Amir Hameed <amirhameed@mac.mynetworksettings.com>
2026-03-05 14:54:14 -0500

    Fix Turbopack root explicit path to resolve Vercel lockfile warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    turbopack: {
+      root: typeof __dirname !== 'undefined' ? __dirname : undefined,
+    }
+  }
 };
 
 export default nextConfig;
